6432e6d6f490cf9dd3ce564f6191c13c84ee2fdc
noppikinatta <noppikitouroku@gmail.com>
2022-06-15 22:49:16 +0900

    feat: add outline types

---
+++ game.go
@@ -0,0 +1,47 @@
+// Copyright 2022 noppikinatta
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebitenginejam01
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/noppikinatta/ebitenginejam01/scene"
+)
+
+type Game struct {
+	current scene.Scene
+}
+
+func NewGame(first scene.Scene) *Game {
+	g := Game{
+		current: first,
+	}
+	return &g
+}
+
+func (g *Game) Update() error {
+	next, ok := g.current.Next()
+	if ok {
+		g.current = next
+	}
+	return g.current.Update()
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.current.Draw(screen)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth / 2, outsideHeight / 2
+}
